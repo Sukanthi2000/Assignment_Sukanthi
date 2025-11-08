2863479a0581496ee01d4a8aa8ef5c81caa0d5c9
“sukanthi2000 <sukanthi2000r@gmail.com>
2025-11-08 20:56:39 +0530

    changes in setup script

--- Assignment_6/app.py
+++ Assignment_6/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, Response
-from prometheus_client import Counter, generate_latest
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
+# Define a Prometheus counter metric
 REQUEST_COUNT = Counter('app_requests_total', 'Total number of requests')
 
 @app.route('/')
@@ -12,7 +13,9 @@ def index():
 
 @app.route('/metrics')
 def metrics():
-    return Response(generate_latest(), mimetype='text/plain')
+    # Return metrics with the correct Prometheus content type
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
+    # Bind to all interfaces so Prometheus can reach it
     app.run(host='0.0.0.0', port=5000)
